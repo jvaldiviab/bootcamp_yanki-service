85760568f7397098cfa1274da2453ef43336155f
JValdivia <juancarlosvaldiviaberrios@gmail.com>
2022-05-05 14:20:55 -0500

    update week 3

---
+++ src/test/java/com/project/yanki/YankiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.yanki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YankiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
